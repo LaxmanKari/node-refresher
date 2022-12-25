4ef39bbf4c3eac6effaba3b701fdd0058a128313
Kari Laxman <laxmankari28@gmail.com>
2022-12-25 16:26:30 +0530

    add first npm package and git ignore file

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 const http = require("http");
 const displayContent = require('./routes'); 
+var colors = require('colors'); 
 
 const server = http.createServer(displayContent);
 
 server.listen(3001);
+
+
+console.log('hello'.yellow);
